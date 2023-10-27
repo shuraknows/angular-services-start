7c850e290c7b846ea4812ebaa5b31bc5cda0470e
Shura Knows <shuraknows@gmail.com>
2023-10-27 14:13:07 +0300

    Wrong way to user services

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
@@ -14,5 +15,9 @@ export class AccountComponent {
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
     console.log('A server status changed, new status: ' + status);
+
+
+    const service = new LoggingService();
+    service.logStatusChange(status);
   }
 }
