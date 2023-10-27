87e03aecc28b21ecc2c293b9c28b952a0ebd38f8
Shura Knows <shuraknows@gmail.com>
2023-10-27 14:23:19 +0300

    Correct usage of service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService],
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
@@ -13,6 +15,9 @@ export class NewAccountComponent {
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+
+    this.loggingService.logStatusChange(accountStatus);
   }
+
+  constructor(private loggingService: LoggingService) {}
 }
